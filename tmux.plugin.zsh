932dbb769a2e006eba1201ecc6785d2d8d30c279
Grigorii Horos <horosgrisa@gmail.com>
2019-07-29 16:26:19 +0300

    Fix tmux

--- tmux.plugin.zsh
+++ tmux.plugin.zsh
@@ -1,22 +1,20 @@
 DEPENDENCES_ARCH+=( tmux )
 DEPENDENCES_DEBIAN+=( tmux )
-
-
 DEPENDENCES_ZSH+=( zpm-zsh/helpers )
 
 if command -v zpm >/dev/null; then
   zpm zpm-zsh/helpers
 fi
 
-if (( $+commands[tmux] )); then
-  if [[ ! "$TMUX_AUTOSTART" == "false" ]] && [[ -n "$SSH_CONNECTION" ]]; then
+if  command -v tmux 2>/dev/null; then
+  if [[ "$TMUX_AUTOSTART" != "false" && -n "$SSH_CONNECTION" ]]; then
     TMUX_AUTOSTART="true"
   fi
 fi
 
 function _tmux_autostart(){
   if [[ "$TMUX_AUTOSTART" == "true" && -z "$TMUX" ]]; then
-    tmux attach || TERM=xterm-256color tmux new
+    (tmux ls | grep -vq attached && tmux -2 at) || TERM=xterm-256color tmux -2
     exit 0
   fi
   add-zsh-hook -d precmd _tmux_autostart
