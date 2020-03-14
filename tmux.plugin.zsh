0eb1449362c5c60d92b5bbab0c4f5a83eb39cbef
Grigorii Horos <horosgrisa@gmail.com>
2020-03-14 20:13:06 +0200

    Optimize

--- tmux.plugin.zsh
+++ tmux.plugin.zsh
@@ -1,10 +1,19 @@
+#!/usr/bin/env zsh
+# Standarized $0 handling, following:
+# https://github.com/zdharma/Zsh-100-Commits-Club/blob/master/Zsh-Plugin-Standard.adoc
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
 DEPENDENCES_ARCH+=( tmux )
 DEPENDENCES_DEBIAN+=( tmux )
-DEPENDENCES_ZSH+=( zpm-zsh/helpers )
 
 if (( $+functions[zpm] )); then
   zpm zpm-zsh/helpers
 fi
+
+if [[ $PMSPEC != *b* ]] {
+  PATH=$PATH:"${0:h}/bin"
+}
 
 if (( $+commands[tmux] )); then
   if [[ "$TMUX_AUTOSTART" != "false" && -n "$SSH_CONNECTION" ]]; then
@@ -12,9 +21,9 @@ if (( $+commands[tmux] )); then
   fi
 fi
 
-function _tmux_autostart(){
+function _tmux_autostart() {
   if [[ "$TMUX_AUTOSTART" == "true" && -z "$TMUX" ]]; then
-    TERM=xterm-256color tmux -2 new-session -A -s main 
+    TERM=xterm-256color tmux -2 new-session -A -s main
     exit 0
   fi
   add-zsh-hook -d precmd _tmux_autostart
@@ -22,8 +31,9 @@ function _tmux_autostart(){
 autoload -Uz add-zsh-hook
 add-zsh-hook precmd _tmux_autostart
 
-if [[ $TMUX_MOTD != false && ! -z $TMUX  && \
-$(\tmux list-windows | wc -l | tr -d ' ') == 1 ]] && \
-( \tmux list-windows | tr -d ' '|grep -q 1panes  ); then
-  _tmux_motd
+if [[ $TMUX_MOTD != false && ! -z $TMUX ]]; then
+  declare -a list_windows; list_windows=( ${(f)"$(command tmux list-windows)"} )
+  if [[ "${#list_windows}" == 1 && "${list_windows}" == *"1 panes"*  ]]; then
+    _tmux_motd
+  fi
 fi
